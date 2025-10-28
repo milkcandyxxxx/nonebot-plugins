7e811c8ea22d97b3419980263ee49a8d9157c025
milkcandy <milkcandyxxxx@gmail.com>
2025-10-28 11:01:11 +0800

    修复int类型报错问题

--- src/plugins/echo.py
+++ src/plugins/echo.py
@@ -6,16 +6,12 @@ __plugin_meta__ = PluginMetadata(
     name="echo",
     description="模仿",
     usage="echo",
-    homepage=None,
     type="application",
-    config=None,
-    supported_adapters=None,
-    extra={},
 )
 
-echo = on_command("echo") # 不设置 permission，所有人可用
+echo = on_command("echo")  # 所有人可用
 
 @echo.handle()
 async def _(bot, event):
-    echo_1 = order(event.get_message())  # 关键：用 str() 转换
-    await echo.finish(echo_1[1])
+    echo_1 = order(event.get_message())  # order 返回可能是 int
+    await echo.finish(str(echo_1[1]))   # 转成字符串再发送
